cbb318aeb9ce4997ce7c889af895fc61f1cd5a0f
Bijita <bijeeetabhattarai@gmail.com>
2025-12-19 02:28:23 +0000

    tailwind integration

--- client/app/Landing.tsx
+++ client/app/Landing.tsx
@@ -1,7 +1,7 @@
 const Landing = () => {
   return (
-    <div>Landing</div>
-  )
-}
+    <div className="bg-black/90 min-h-screen text-white font-extrabold flex justify-center items-center">Landing</div>
+  );
+};
 
-export default Landing
+export default Landing;
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react()],
-  esbuild: {  jsx: "automatic" },
-  root: './client',
-  build: {outDir: "../build/client",  emptyOutDir: true },
-  server: { host: "127.0.0.1",  port: 9000, 
-    proxy: { "/api": { target: "http://127.0.0.1:9001", changeOrigin: true, secure: false, } }
-  }
-})
+  plugins: [react(), tailwindcss()],
+  esbuild: { jsx: "automatic" },
+  root: "./client",
+  build: { outDir: "../build/client", emptyOutDir: true },
+  server: {
+    host: "127.0.0.1",
+    port: 9000,
+    proxy: { "/api": { target: "http://127.0.0.1:9001", changeOrigin: true, secure: false } },
+  },
+});
